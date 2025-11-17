b60c87eac06efc99a14a3d7828ea7f295ec8ec60
Daniel Żak <icobragroup@gmail.com>
2025-11-17 10:17:45 +0100

    biding events moved to a separate function

--- js/script.js
+++ js/script.js
@@ -27,6 +27,23 @@
 		render();
 	}
 
+	const bindEvents = () => {
+		const removeButtons = document.querySelectorAll(".js-remove");
+		removeButtons.forEach((removeButton, index) => {
+			removeButton.addEventListener("click", () => {
+				removeTask(index);
+			});
+		});
+
+		const toggleDoneButtons = document.querySelectorAll(".js-done");
+		toggleDoneButtons.forEach((toggleDoneButton, index) => {
+			toggleDoneButton.addEventListener("click", () => {
+				toggleTaskDone(index);
+			});
+		});
+
+	}
+
 	const render = () => {
 		let htmlString = "";
 		for (const task of tasks) {
@@ -42,20 +59,7 @@
 		}
 		document.querySelector(".js-task").innerHTML = htmlString;
 
-		const removeButtons = document.querySelectorAll(".js-remove");
-		removeButtons.forEach((removeButton, index) => {
-			removeButton.addEventListener("click", () => {
-				removeTask(index);
-			});
-		});
-
-		const toggleDoneButtons = document.querySelectorAll(".js-done");
-		toggleDoneButtons.forEach((toggleDoneButton, index) => {
-			toggleDoneButton.addEventListener("click", () => {
-				toggleTaskDone(index);
-			});
-		});
-
+		bindEvents();
 	};
 
 
