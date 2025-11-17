b7b00dcc89accaaae86429c49c6f86f97e9ff4bb
Daniel Żak <icobragroup@gmail.com>
2025-11-17 08:57:58 +0100

    initial comit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+{
+	const tasks = [
+		{
+			content: "nagrać lekcję",
+			done: false,
+		},
+		{
+			content: "zjeść pierogi",
+			done: true,
+		},
+	];
+
+	const render = () => {
+		let htmlString = "";
+		for (const task of tasks) {
+			htmlString +=
+				`
+            <li>
+            ${task.content}
+            </li>
+            `;
+		}
+		document.querySelector(".js-task").innerHTML = htmlString;
+	}
+	const init = () => {
+		render();
+	};
+
+	init();
+}
