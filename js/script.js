a867991e731cbea4ee71bd8d40b10428ce071753
Daniel Żak <icobragroup@gmail.com>
2025-11-17 09:54:51 +0100

    remove task

--- js/script.js
+++ js/script.js
@@ -9,6 +9,18 @@
 			done: true,
 		},
 	];
+	const addNewTask = (newTaskContent) => {
+		tasks.push(
+			{
+				content: newTaskContent,
+			});
+		render();
+	};
+
+	const removeTask = (taskIndex) => {
+		tasks.splice(taskIndex, 1);
+		render();
+	}
 
 	const render = () => {
 		let htmlString = "";
@@ -17,20 +29,22 @@
 				`
         <li 
 				${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+				<button class="js-remove">usuń</button>
 				${task.content}
 				</li>
 		`;
 		}
 		document.querySelector(".js-task").innerHTML = htmlString;
+
+		const removeButtons = document.querySelectorAll(".js-remove");
+		removeButtons.forEach((removeButton, index) => {
+			removeButton.addEventListener("click", () => {
+				removeTask(index);
+			});
+		})
 	};
 
-	const addNewTask = (newTaskContent) => {
-		tasks.push(
-			{
-				content: newTaskContent,
-			});
-		render();
-	};
+
 
 	const onFormSubmit = (event) => {
 		event.preventDefault();
